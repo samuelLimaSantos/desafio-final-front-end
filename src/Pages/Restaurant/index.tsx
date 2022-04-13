3158764676a6caacfcdca4bc13e969c6fe76b04b
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-13 11:13:06 -0300

    adicionando um modelo de exemplo na pagina restaurante

--- src/Pages/Restaurant/index.tsx
+++ src/Pages/Restaurant/index.tsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import Back from "../../assets/icons/button-back.svg";
+import { RestaurantReady } from "../../Mocks/input";
+import "./style.css";
 const Restaurant = () => {
   const navigation = useNavigate();
+
+  const { name, urlImage, description, urlCover } = RestaurantReady[3];
+
   return (
     <div className="container">
       <div
@@ -11,6 +16,19 @@ const Restaurant = () => {
         className="button-back"
       >
         <img src={Back} alt="" />
+      </div>
+      <div className="container-rest">
+        <img className="url-cover" src={urlCover} />
+        <div className="card-info">
+          <div className="description">
+            <img src={urlImage} alt="" />
+            <h3>{name}</h3>
+            <h5>{description}</h5>
+          </div>
+          <div>
+            <h1>cardapio</h1>
+          </div>
+        </div>
       </div>
     </div>
   );

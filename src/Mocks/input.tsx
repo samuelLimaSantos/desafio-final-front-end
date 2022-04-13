c4ba5fd410a23f6067da09111ceeae19ee424c38
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-13 07:43:06 -0300

    Adicionado Mocks dos Restaurantes

--- src/Mocks/input.tsx
+++ src/Mocks/input.tsx
@@ -58,3 +58,62 @@ export const InputProduct = [
   },
   { name: "Cebola", price: 1.5 },
 ];
+
+export const RestaurantReady = [
+  {
+    name: "Sushi Home",
+    urlImage:
+      "https://thumbs.dreamstime.com/b/modelo-do-logotipo-sushi-de-restaurante-japon%C3%AAs-comida-estilo-simples-design-geom%C3%A9trico-ilustra%C3%A7%C3%A3o-isolada-vetor-209975075.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Italian Food",
+    urlImage:
+      "https://images.vexels.com/media/users/3/174870/raw/562bac05b93c71d7bc06be711af445b3-modelo-de-logotipo-de-restaurante-italiano.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Maluhia",
+    urlImage:
+      "https://media-cdn.tripadvisor.com/media/photo-s/12/23/3d/4e/comida-vegetariana-e.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Pastel Chef",
+    urlImage:
+      "https://blog.agenciadosite.com.br/wp-content/uploads/2017/02/Pastel-Chef-L-SF-e1486724164401.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Temperô",
+    urlImage:
+      "https://nacaodesign.com.br/wp-content/uploads/2015/06/logo-tempero.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Zé Coxinha",
+    urlImage:
+      "https://mir-s3-cdn-cf.behance.net/projects/404/036da8119104879.Y3JvcCwxMzgwLDEwODAsMjcwLDA.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Ávila",
+    urlImage:
+      "https://i.pinimg.com/originals/74/13/93/741393fd17c877fcd7f7769094d47bb3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+  {
+    name: "Luiza Doces",
+    urlImage:
+      "https://www.vintecontologos.com.br/wp-content/uploads/2017/05/luiza-doces-logo.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+  },
+];

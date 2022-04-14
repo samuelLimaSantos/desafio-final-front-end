02c9bc628eead85fb990bfd13c7eaf3acc598dfc
Paulo Marques <paulomarques@rethinks-MacBook-Pro.local>
2022-04-13 22:29:13 -0300

    Adicionando a parte do axios de get e post

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from "react";
-import Shop from "../../components/Shop";
 
-import API from "../../API";
-import RestaurantForm from "../RestaurantForm";
+import axios from "../../service";
+
 import Header from "../../components/Header";
 import Destak from "../../components/Destak";
 
 const Home = () => {
-  const [listRestaurants, setListRestaurants] = useState([]);
+  const [detach, setDetach] = useState([]);
+  const [rest, setRest] = useState([]);
 
-  /*  
-    useEffect(() => {
+  useEffect(() => {
     const loadAll = async () => {
       //pegando a lista inteira
-      let list = await API.getHomeList();
-      setListRestaurants(list)
-
-      loadAll();
+      let list = await axios.get("/rest/restaurants");
+      const detach = list.data.slice(0, 8);
+      const rest = list.data.slice(8);
+      setDetach(detach);
+      setRest(rest);
+    };
+    loadAll();
   }, []);
-  
-*/
+
   return (
     <div>
       <Header gray />
-      <Destak />
+      <Destak detach={detach} />
       {/* <section className='lists'>
         {listRestaurants.map((item, key) =>(
           <Shop key={key} title={item.title} items={item.items} />
--- src/components/Destak/index.tsx
+++ src/components/Destak/index.tsx
@@ -1,13 +1,30 @@
 import { RestaurantReady } from "../../Mocks/input";
 import Selo from "../../assets/icons/selo.svg";
 import "./style.css";
-const Destak = () => {
+import { useNavigate } from "react-router-dom";
+
+interface Detach {
+  urlLogo: string;
+  id: string;
+}
+
+interface DestakProps {
+  detach: Detach[];
+}
+
+const Destak: React.FC<DestakProps> = ({ detach }: DestakProps) => {
+  const navigation = useNavigate();
   return (
     <div className="container-restaurant">
-      {RestaurantReady.map((item) => (
+      {detach.map((item) => (
         <div className="restaurant-img">
           <img id="selo" src={Selo} />
-          <img src={item.urlImage} />
+          <img
+            src={item.urlLogo}
+            onClick={() =>
+              navigation(`/restaurant`, { state: { id: item.id } })
+            }
+          />
         </div>
       ))}
     </div>
